737bea74bd7ed3302c2078758f8e611326b0616d
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-03 02:04:13 +0100

    style: :lipstick:

--- domain/aggregate/customer/entities/validate.go
+++ domain/aggregate/customer/entities/validate.go
@@ -10,7 +10,7 @@ var (
 	ErrInvalidPerson = errors.New("a customer has to have an valid person")
 )
 
-func (custumer *Custumer) IsValid() error {
+func (custumer *Customer) IsValid() error {
 	_, err := govalidator.ValidateStruct(custumer)
 	if err != nil {
 		return err
