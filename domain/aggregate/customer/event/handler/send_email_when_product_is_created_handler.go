341a9b0d4b73c6d5c2e4990a7ed8bc7df3650a27
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-03 02:16:02 +0100

    feat: :construction: send email customer event in progress

---
+++ domain/aggregate/customer/event/handler/send_email_when_product_is_created_handler.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"bookstore/domain/event"
+	"fmt"
+)
+
+func SendEmailWhenCustomerIsCreatedHandler() func(*event.EventType) {
+	fmt.Println("Sending email to .....")
+	return func(m *event.EventType) {
+
+		fmt.Println(m.EventData)
+	}
+}
+
+//type name struct {
+//	Name string
+//}
+//productOutput := name{}
+//		productJson, _ := json.Marshal(m.EventData)
+//		json.Unmarshal(productJson, &productOutput)
+//		fmt.Println(productOutput.Name)
