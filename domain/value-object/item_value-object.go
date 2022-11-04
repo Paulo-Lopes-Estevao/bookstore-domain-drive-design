8dfd4fc5a9bee26eb1fc979adbd05d91ce8fc0d7
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-04 14:52:27 +0000

    feat: :construction:

--- domain/value-object/item_value-object.go
+++ domain/value-object/item_value-object.go
@@ -1,1 +1,33 @@
 package valueobject
+
+import "github.com/asaskevich/govalidator"
+
+type ItemValueObject struct {
+	Name        string  `valid:"required"`
+	Price       float64 `valid:"required"`
+	Quantity    int     `valid:"required"`
+	Description string  `valid:"-"`
+}
+
+func NewItemValueObject(name, description string, price float64, quantity int) (*ItemValueObject, error) {
+	t := ItemValueObject{
+		Name:        name,
+		Description: description,
+		Price:       price,
+		Quantity:    quantity,
+	}
+
+	err := t.IsValid()
+	if err != nil {
+		return nil, err
+	}
+	return &t, nil
+}
+
+func (itemValueObject *ItemValueObject) IsValid() error {
+	_, err := govalidator.ValidateStruct(itemValueObject)
+	if err != nil {
+		return err
+	}
+	return nil
+}
