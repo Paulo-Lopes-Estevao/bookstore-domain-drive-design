38aac4e111802c5ec8f50479ee37a3c80eb4c8d6
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-05 20:13:49 +0100

    refactor: :recycle:

--- domain/aggregate/order.go
+++ domain/aggregate/order.go
@@ -2,27 +2,29 @@ package aggregate
 
 import (
 	"bookstore/domain/entities/order"
-	"bookstore/domain/services"
+	valueobject "bookstore/domain/value-object"
 )
 
 type Order struct {
-	customer Customer
-	items    []*order.OrderItem
+	customerID Customer
+	items      []*order.OrderItem
+	Address    valueobject.Address
 }
 
-func NewOrder(customer Customer, items ...*order.OrderItem) (*Order, error) {
-	order, err := services.PlaceOrder(customer.ID.String(), items...)
-	if err != nil {
+func NewOrder(customer Customer, address valueobject.Address, items ...*order.OrderItem) (*Order, error) {
+	order := &Order{
+		customerID: customer,
+		items:      items,
+		Address:    address,
+	}
+	if err := address.IsValid(); err != nil {
 		return nil, err
 	}
-	return &Order{
-		customer: customer,
-		items:    order.Item,
-	}, nil
+	return order, nil
 }
 
 func (o Order) GetCustomer() Customer {
-	return o.customer
+	return o.customerID
 }
 
 func (o Order) GetItems() []*order.OrderItem {
