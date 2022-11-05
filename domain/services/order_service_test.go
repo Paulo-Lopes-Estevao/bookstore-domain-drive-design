00d95ddfeb3b16ac0edd6d8ce722295d05c93b96
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-05 20:16:45 +0100

    test: :white_check_mark:

---
+++ domain/services/order_service_test.go
@@ -0,0 +1,20 @@
+package services_test
+
+import (
+	"bookstore/domain/entities/order"
+	"bookstore/domain/services"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTotalOrderPrice(t *testing.T) {
+	orderItem := order.NewOrderItem("Harry Potter", "History", 400, 3)
+
+	order, err := order.NewOrder("123", orderItem)
+	assert.NoError(t, err)
+
+	total := services.TotalOrderPrice(order)
+	assert.Equal(t, 1200, total)
+
+}
