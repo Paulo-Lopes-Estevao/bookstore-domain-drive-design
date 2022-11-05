79724a8e990ee57d1a29a37a3aa540f4d258571c
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-05 20:15:31 +0100

    refactor: :recycle: change to total order price

--- domain/services/order_service.go
+++ domain/services/order_service.go
@@ -2,16 +2,9 @@ package services
 
 import (
 	"bookstore/domain/entities/order"
-	"errors"
 )
 
-func PlaceOrder(customerId string, items ...*order.OrderItem) (*order.Order, error) {
-	if len(items) == 0 {
-		errors.New("Order must have at least one item")
-	}
-	order, err := order.NewOrder(customerId, items...)
-	if err != nil {
-		return nil, err
-	}
-	return order, nil
+func TotalOrderPrice(order *order.Order) int {
+	total := order.Total()
+	return total
 }
