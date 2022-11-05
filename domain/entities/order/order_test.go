6bd4d7bae476d787b6c3f53935a4c4439a61a471
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-05 17:07:37 +0100

    test: :white_check_mark:

--- domain/entities/order/order_test.go
+++ domain/entities/order/order_test.go
@@ -8,14 +8,14 @@ import (
 )
 
 func TestOderwhenOrderIdIsEmptyWithCustomerId(t *testing.T) {
-	_, err := order.NewOrder("123", "", nil)
+	_, err := order.NewOrder("123", nil)
 	if assert.Error(t, err) {
 		assert.Equal(t, "Costumer ID cannot be empty", err.Error())
 	}
 }
 
 func TestOderwhenOrderItemsIsEmpty(t *testing.T) {
-	_, err := order.NewOrder("123", "123", &order.OrderItem{})
+	_, err := order.NewOrder("123", &order.OrderItem{})
 	if assert.Error(t, err) {
 		assert.Equal(t, "OrderItem ID cannot be empty", err.Error())
 	}
@@ -25,7 +25,7 @@ func TestOderwhenOrderCalculateTotal(t *testing.T) {
 	orderItem := order.NewOrderItem("Harry Potter", "History", 400, 2)
 	orderItem2 := order.NewOrderItem("Harry Potter", "History", 300, 2)
 
-	order2, err := order.NewOrder("123", "123", orderItem, orderItem2)
+	order2, err := order.NewOrder("123", orderItem, orderItem2)
 	assert.Nil(t, err)
 
 	assert.Equal(t, order2.Total(), 1400)
@@ -35,7 +35,7 @@ func TestOderwhenOrderCalculateTotal(t *testing.T) {
 func TestOderwhenOrderItemQtyIsLessOrEqualZero0(t *testing.T) {
 	orderItem := order.NewOrderItem("Harry Potter", "History", 400, 0)
 
-	_, err := order.NewOrder("123", "123", orderItem)
+	_, err := order.NewOrder("123", orderItem)
 
 	if assert.Error(t, err) {
 		assert.Equal(t, "OrderItem Quantity cannot be zero", err.Error())
