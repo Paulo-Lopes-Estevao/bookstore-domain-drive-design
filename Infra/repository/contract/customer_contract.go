aa589f502f992297183d7b784f3d8c3d082651c9
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-06 23:44:45 +0100

    feat: :sparkles: add customer contract

---
+++ Infra/repository/contract/customer_contract.go
@@ -0,0 +1,24 @@
+package contract
+
+import (
+	"bookstore/Infra/repository"
+	repoDomain "bookstore/domain/repository"
+
+	"github.com/jinzhu/gorm"
+)
+
+type ICustomerContractRepository interface {
+	CustomerContractRepository() repoDomain.ICustomerRepository
+}
+
+type CustomerContract struct {
+	db *gorm.DB
+}
+
+func NewCustomerContractRepository(db *gorm.DB) ICustomerContractRepository {
+	return &CustomerContract{db: db}
+}
+
+func (c *CustomerContract) CustomerContractRepository() repoDomain.ICustomerRepository {
+	return repository.NewCustomerRepository(c.db)
+}
