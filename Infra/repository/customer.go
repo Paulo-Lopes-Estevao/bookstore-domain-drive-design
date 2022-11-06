b25dc758eb0c84259405783029325489cfbb49ef
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-06 23:47:49 +0100

    feat: :sparkles: add customer repository

---
+++ Infra/repository/customer.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"bookstore/domain/aggregate"
+
+	"github.com/jinzhu/gorm"
+)
+
+type CustomerRepository struct {
+	db *gorm.DB
+}
+
+func NewCustomerRepository(db *gorm.DB) *CustomerRepository {
+	return &CustomerRepository{db: db}
+}
+
+func (r *CustomerRepository) Create(entity *aggregate.Customer) error {
+	return r.db.Create(entity).Error
+}
+
+func (r *CustomerRepository) Update(entity *aggregate.Customer) error {
+	return r.db.Save(entity).Error
+}
+
+func (r *CustomerRepository) Find(id string) (*aggregate.Customer, error) {
+	var customer aggregate.Customer
+	if err := r.db.Where("id =?", id).Find(&customer).Error; err != nil {
+		return nil, err
+	}
+	return &customer, nil
+}
+
+func (r *CustomerRepository) FindAll(filter *aggregate.Customer) ([]aggregate.Customer, error) {
+	var customers []aggregate.Customer
+	if err := r.db.Where(filter).Find(&customers).Error; err != nil {
+		return nil, err
+	}
+	return customers, nil
+}
+
+func (r *CustomerRepository) Delete(id string) error {
+	return r.db.Where("id =?", id).Delete(&aggregate.Customer{}).Error
+}
