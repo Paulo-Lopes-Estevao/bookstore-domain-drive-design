8db3058523ca79b9ea79a83524ed63a5711111e1
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-06 15:16:07 +0100

    refactor: :recycle:

--- domain/aggregate/order.go
+++ domain/aggregate/order.go
@@ -6,27 +6,61 @@ import (
 )
 
 type Order struct {
-	customerID Customer
-	items      []*order.OrderItem
-	Address    valueobject.Address
+	CustomerID string
+	OrderItem  []*order.OrderItem
+	Address    *valueobject.Address
 }
 
-func NewOrder(customer Customer, address valueobject.Address, items ...*order.OrderItem) (*Order, error) {
-	order := &Order{
-		customerID: customer,
-		items:      items,
-		Address:    address,
-	}
-	if err := address.IsValid(); err != nil {
+func NewOrder(orderAggregate Order) (*Order, error) {
+
+	err := orderAggregate.AddAddress()
+	if err != nil {
 		return nil, err
 	}
-	return order, nil
+
+	err = orderAggregate.AddOrderItem()
+	if err != nil {
+		return nil, err
+	}
+
+	order, err := order.NewOrder(orderAggregate.CustomerID, orderAggregate.OrderItem...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &Order{
+		CustomerID: order.CostumerID,
+		OrderItem:  orderAggregate.OrderItem,
+		Address:    orderAggregate.Address,
+	}, nil
 }
 
-func (o Order) GetCustomer() Customer {
-	return o.customerID
+func (o Order) GetAddress() *valueobject.Address {
+	return o.Address
 }
 
-func (o Order) GetItems() []*order.OrderItem {
-	return o.items
+func (o Order) GetCustomerID() string {
+	return o.CustomerID
+}
+
+func (o Order) GetOrderItem() []*order.OrderItem {
+	return o.OrderItem
+}
+
+func (orderItem *Order) AddOrderItem() error {
+	for _, v := range orderItem.OrderItem {
+		_, err := order.NewOrderItem(v.Name, v.Description, v.Price, v.Quantity)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (orderAddress *Order) AddAddress() error {
+	_, err := valueobject.NewAddress(orderAddress.Address.Province, orderAddress.Address.County, orderAddress.Address.Street, orderAddress.Address.Number, orderAddress.Address.Country)
+	if err != nil {
+		return err
+	}
+	return nil
 }
