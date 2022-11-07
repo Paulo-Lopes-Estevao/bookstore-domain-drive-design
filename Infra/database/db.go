4d0dc56539432c15e6a56f4e2fa91de57c15985a
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-07 14:56:03 +0000

    feat: :sparkles:

---
+++ Infra/database/db.go
@@ -0,0 +1,27 @@
+package database
+
+import (
+	"bookstore/Infra/database/entities"
+	"fmt"
+
+	"github.com/jinzhu/gorm"
+)
+
+// drive: mysql, sqlite3, postgres
+func DB(drive string, dsn string) (*gorm.DB, error) {
+
+	db, err := gorm.Open(drive, dsn)
+
+	if err != nil {
+
+			return nil, err
+
+	}
+
+	db.AutoMigrate(&entities.Customer{}, &entities.OrderItem{}, &entities.Order{}, &entities.Category{}, &entities.Product{})
+
+	fmt.Println("Connection Opened to Database")
+
+	return db, nil
+
+}
