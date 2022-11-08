0b7b4cc45c631caa41c6e9f809c42b73b2c2e4cd
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-08 11:25:09 +0100

    test: :white_check_mark:

--- Infra/database/repository/product_test.go
+++ Infra/database/repository/product_test.go
@@ -1,1 +1,49 @@
 package repository_test
+
+import (
+	"bookstore/Infra/database"
+	"bookstore/Infra/database/postgresql"
+	"bookstore/Infra/database/repository"
+	"bookstore/domain/aggregate"
+	"log"
+	"path/filepath"
+	"runtime"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+)
+
+func init() {
+	_, b, _, _ := runtime.Caller(0)
+	basepath := filepath.Dir(b)
+
+	err := godotenv.Load(basepath + "/../../../.env")
+
+	if err != nil {
+		log.Fatalf("Error loading .env files")
+	}
+}
+
+func TestCreateCategory(t *testing.T) {
+
+	dsn := postgresql.PostgreSQLConnect()
+	db, err := database.DB("postgres", dsn)
+	assert.Nil(t, err)
+
+	defer db.Close()
+
+	productRepo := repository.NewProductRepository(db)
+	product := aggregate.Product{}
+	product.Name = "Harry Potter"
+	product.Price = 400
+
+	errCategory := productRepo.CreateCategory("Crônica")
+	assert.Nil(t, errCategory)
+
+	errProduct := productRepo.Create(&product)
+	if errProduct != nil {
+		t.Errorf("Failed to repository create product: %v", errProduct)
+	}
+
+}
