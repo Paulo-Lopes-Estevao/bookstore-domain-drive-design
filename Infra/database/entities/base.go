46f4f60f365b1556d1a736e5095d9de6a93c4d76
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-12 02:36:10 +0100

    refactor: :fire: remove id

--- Infra/database/entities/base.go
+++ Infra/database/entities/base.go
@@ -2,12 +2,9 @@ package entities
 
 import (
 	"time"
-
-	"github.com/google/uuid"
 )
 
 type Base struct {
-	ID        uuid.UUID `gorm:"primary_key"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt *time.Time `sql:"index"`
