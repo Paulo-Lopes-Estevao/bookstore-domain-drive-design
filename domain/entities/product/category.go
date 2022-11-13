aaa2aabf67af164b7f9727e1913b139b5bfdcfd7
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-13 18:40:47 +0100

    refactor: :recycle:

--- domain/entities/product/category.go
+++ domain/entities/product/category.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"errors"
 	"time"
 
 	"github.com/google/uuid"
@@ -11,4 +12,27 @@ type Category struct {
 	Name      string
 	CreatedAt time.Time
 	UpdatedAt time.Time
+}
+
+func NewCategory(name string) (*Category, error) {
+	category := &Category{
+		ID:        uuid.New(),
+		Name:      name,
+		CreatedAt: time.Now(),
+	}
+	err := category.Validate()
+	if err != nil {
+		return nil, err
+	}
+	return category, nil
+}
+
+func (c *Category) Validate() error {
+	if c.ID == uuid.Nil {
+		return errors.New("id must not be empty")
+	}
+	if c.Name == "" {
+		return errors.New("name must not be empty")
+	}
+	return nil
 }
