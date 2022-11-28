6da8e9d819c8900910ee6b7f2679071fae36fc93
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-28 23:59:01 +0100

    refactor: :recycle:

--- domain/factory/order_factory.go
+++ domain/factory/order_factory.go
@@ -4,17 +4,53 @@ import (
 	"bookstore/domain/aggregate"
 )
 
-type OrderFactory struct {
-	Order aggregate.Order
+type (
+	// AggregateFactory is the factory interface for Aggregate
+	AggregateFactory interface {
+		CreateOrder(orderFactory *aggregate.OrderAggregate) (*aggregate.OrderAggregate, error)
+		CreateOrderItem(orderItemFactory *aggregate.OrderItemAggregate) (aggregate.OrderItemAggregate, error)
+	}
+
+	// AggregateFactoryImpl is the factory implementation for Aggregate
+	AggregateFactoryImpl struct {
+		//aggregateRepository aggregate.Repository
+		aggregateOrder     aggregate.OrderAggregate
+		aggregateOrderItem aggregate.OrderItemAggregate
+		//aggregateService    aggregate.Service
+		//logger              Logger
+	}
+)
+
+// NewAggregateFactory is the factory constructor for Aggregate
+func NewAggregateFactory(aggregateOrder aggregate.OrderAggregate) AggregateFactory {
+	return &AggregateFactoryImpl{
+		aggregateOrder: aggregateOrder,
+	}
+
 }
 
-func CreateOrder(orderFactory OrderFactory) (*OrderFactory, error) {
-	order, err := aggregate.NewOrder(orderFactory.Order)
+// CreateOrder is the factory method for CreateOrder
+func (factory *AggregateFactoryImpl) CreateOrder(orderFactory *aggregate.OrderAggregate) (*aggregate.OrderAggregate, error) {
+	order, err := aggregate.NewOrder(orderFactory, &orderFactory.Item)
 	if err != nil {
 		return nil, err
 	}
 
-	return &OrderFactory{
-		Order: *order,
+	return order, nil
+}
+
+// CreateOrderItem is the factory method for CreateOrderItem
+func (factory *AggregateFactoryImpl) CreateOrderItem(orderItemFactory *aggregate.OrderItemAggregate) (aggregate.OrderItemAggregate, error) {
+	orderItem, err := aggregate.NewOrderItem(orderItemFactory)
+	if err != nil {
+		return aggregate.OrderItemAggregate{}, err
+	}
+	return aggregate.OrderItemAggregate{
+		ID:          orderItem.ID,
+		ProductID:   orderItem.ProductID,
+		Name:        orderItem.Name,
+		Description: orderItem.Description,
+		Quantity:    orderItem.Quantity,
+		Price:       orderItem.Price,
 	}, nil
 }
