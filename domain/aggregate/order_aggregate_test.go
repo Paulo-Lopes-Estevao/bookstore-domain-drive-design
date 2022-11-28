0cb87bc5e9f0017eef7dbb0a2163a6b94dadec05
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-11-28 23:52:53 +0100

    test: :white_check_mark:

---
+++ domain/aggregate/order_aggregate_test.go
@@ -0,0 +1,36 @@
+package aggregate_test
+
+import (
+	"bookstore/domain/aggregate"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCustomerOrder(t *testing.T) {
+	orderAggregate := &aggregate.OrderAggregate{}
+	orderItemAggregate := &aggregate.OrderItemAggregate{}
+
+	orderAggregate.CostumerID = "123"
+
+	orderAggregate.Address.Province = "Luanda"
+	orderAggregate.Address.County = "Luanda"
+	orderAggregate.Address.Street = "Morro Bento ||"
+	orderAggregate.Address.Number = 1234
+	orderAggregate.Address.Country = "Angola"
+
+	orderItemAggregate.Name = "Harry Potter"
+	orderItemAggregate.Description = "Description"
+	orderItemAggregate.Quantity = 1
+	orderItemAggregate.Price = 7000
+	orderItemAggregate.ProductID = uuid.New().String()
+
+	orderItem, err := aggregate.NewOrderItem(orderItemAggregate)
+	assert.Nil(t, err)
+
+	ordered, err := aggregate.NewOrder(orderAggregate, orderItem)
+	assert.Nil(t, err)
+	assert.Equal(t, ordered.CostumerID, "123")
+
+}
