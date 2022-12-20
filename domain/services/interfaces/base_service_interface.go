63ca7c4b76b6b904dcda218fab62f8010f8cc64e
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-20 23:20:22 +0100

    feat: :sparkles: add inteface base domain service

---
+++ domain/services/interfaces/base_service_interface.go
@@ -0,0 +1,9 @@
+package interfaces
+
+type ICustumerService[T any] interface {
+	Create(entity *T) error
+	Update(entity *T) error
+	GetById(id string) (*T, error)
+	GetAll(page int, pageNumber int) (map[string]*T, error)
+	Remove(id string) error
+}
