45dbc4ec29cad49bba660bd9dfa305b82ccbf94d
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-21 10:50:39 +0100

    refactor: :recycle: refator generic base service

--- domain/services/interfaces/base_service_interface.go
+++ domain/services/interfaces/base_service_interface.go
@@ -4,6 +4,6 @@ type IBaseService[T any] interface {
 	Create(entity *T) error
 	Update(entity *T) error
 	GetById(id string) (*T, error)
-	GetAll(page int, pageNumber int) (map[string]*T, error)
+	GetAll() ([]*T, error)
 	Remove(id string) error
 }
