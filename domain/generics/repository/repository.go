f2f775d0babf2918ab0baffe5e7192c6ba50dd7a
Paulo Lopes Estevão <pl1745240@gmail.com>
2022-12-21 10:48:54 +0100

    refactor: :recycle:

--- domain/generics/repository/repository.go
+++ domain/generics/repository/repository.go
@@ -4,6 +4,6 @@ type IRepository[T any] interface {
 	Create(entity *T) error
 	Update(entity *T) error
 	Find(id string) (*T, error)
-	FindAll() ([]T, error)
+	FindAll() ([]*T, error)
 	Delete(id string) error
 }
