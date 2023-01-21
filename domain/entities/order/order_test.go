c7f142090f8d3bc2800d11665e79a476bd46019b
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-21 11:51:23 +0100

    feat: add orderID

--- domain/entities/order/order_test.go
+++ domain/entities/order/order_test.go
@@ -23,8 +23,8 @@ func TestOderwhenOrderItemsIsEmpty(t *testing.T) {
 }
 
 func TestOderwhenOrderCalculateTotal(t *testing.T) {
-	orderItem, _ := order.NewOrderItem("Harry Potter", "History", uuid.New(), 400, 2)
-	orderItem2, _ := order.NewOrderItem("Harry Potter", "History", uuid.New(), 300, 2)
+	orderItem, _ := order.NewOrderItem("Harry Potter", "History", uuid.New(), 400, 2, uuid.New())
+	orderItem2, _ := order.NewOrderItem("Harry Potter", "History", uuid.New(), 300, 2, uuid.New())
 
 	order2, err := order.NewOrder("123", orderItem, orderItem2)
 	assert.Nil(t, err)
@@ -34,7 +34,7 @@ func TestOderwhenOrderCalculateTotal(t *testing.T) {
 }
 
 func TestOderwhenOrderItemQtyIsLessOrEqualZero0(t *testing.T) {
-	_, err := order.NewOrderItem("Harry Potter", "History", uuid.New(), 400, 0)
+	_, err := order.NewOrderItem("Harry Potter", "History", uuid.New(), 400, 0, uuid.New())
 
 	if assert.Error(t, err) {
 		assert.Equal(t, "OrderItem Quantity cannot be zero", err.Error())
