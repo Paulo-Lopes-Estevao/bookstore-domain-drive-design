fc58f6037d86ed9477bc20548faf911cc58ed75b
Paulo Lopes Estevão <pl1745240@gmail.com>
2023-01-21 03:22:53 +0100

    refactor: :recycle: change to FindByID

--- domain/generics/repository/repository.go
+++ domain/generics/repository/repository.go
@@ -3,7 +3,7 @@ package repository
 type IRepository[T any] interface {
 	Create(entity *T) error
 	Update(entity *T) error
-	Find(id string) (*T, error)
+	FindByID(id string) (*T, error)
 	FindAll() ([]*T, error)
 	Delete(id string) error
 }
